7a3befe127dbecb892005b300634c23071ceba01
Paolo96700 <paolo.falcoapp@gmail.com>
2023-04-04 16:07:57 +0200

    Correzioni

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const name = prompt('scrivi il tuo nome');
 
 const surname = prompt('scrivi il tuo cognome');
 
-const color = prompt(' il tuo colore preferito')
+const color = prompt(' il tuo colore preferito');
 
 document.getElementById('result').innerHTML = (name + surname + color + '23');
