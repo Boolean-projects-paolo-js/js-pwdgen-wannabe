5307e1a3bbbd01afffa93c1cd383650f840266db
Paolo96700 <paolo.falcoapp@gmail.com>
2023-04-04 14:48:41 +0200

    Esercizio

---
+++ script.js
@@ -0,0 +1,7 @@
+const name = prompt('scrivi il tuo nome');
+
+const surneme = prompt('scrivi il tuo cognome');
+
+const color = prompt(' il tuo colore preferito')
+
+// document.writeln("name + surname + color + '23')
