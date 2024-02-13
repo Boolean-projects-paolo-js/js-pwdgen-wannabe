46f0e3c52e1f82a896a11bf77bdef34786228ae6
Paolo Falco <paolo.falcoapp@gmail.com>
2024-02-13 16:12:20 +0100

    fix script

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ const surname = prompt('scrivi il tuo cognome');
 
 const color = prompt(' il tuo colore preferito');
 
-document.getElementById('result').innerHTML = (name + surname + color + '23');
+document.getElementById('result').innerHTML = ("il tuo nome è: " + name + "<br>" +
+                                                "il tuo cognome è: " + surname + "<br>" +
+                                                "il tuo colore preferito è: " + color);
